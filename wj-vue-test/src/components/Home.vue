57973a86a1a62803e220005b001531e83a37b559
Jia\Haha <jiajiajiajia10003@163.com>
2019-11-26 00:23:24 +0800

    前后端分离项目：前端工程：新建一个组件Home.vue，作为导航栏的父组件

---
+++ wj-vue-test/src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <!-- 把导航栏，放入Home组件中-->
+      <nav-menu></nav-menu>
+      <router-view/>
+    </div>
+</template>
+
+<script>
+import NavMenu from './common/NavMenu'
+export default {
+  name: 'Home',
+  components: {NavMenu}
+}
+</script>
+
+<style scoped>
+
+</style>
