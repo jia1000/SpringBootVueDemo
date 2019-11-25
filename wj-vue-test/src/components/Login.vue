8be83e93056a32734345978b8af1d91c3906920b
jiaYF <jiajiajiajia10003@163.com>
2019-11-25 20:51:48 +0800

    前后端分离项目：前端工程：设置背景。 方法：在 <el-form> 标签的外又添加了一个父标签 <body>，是为了覆盖掉浏览器（用户代理）的默认样式。

--- wj-vue-test/src/components/Login.vue
+++ wj-vue-test/src/components/Login.vue
@@ -1,19 +1,21 @@
 <template>
-  <el-form class="login-container" label-position="left"
-           label-width="0px">
-    <h3 class="login_title">系统登录</h3>
-    <el-form-item>
-      <el-input type="text" v-model="loginForm.username"
-                auto-complete="off" placeholder="账号"></el-input>
-    </el-form-item>
-    <el-form-item>
-      <el-input type="password" v-model="loginForm.password"
-                auto-complete="off" placeholder="密码"></el-input>
-    </el-form-item>
-    <el-form-item style="width: 100%">
-      <el-button type="primary" style="width: 100%;background: #505458;border: none" v-on:click="login">登录</el-button>
-    </el-form-item>
-  </el-form>
+  <body id="poster">
+    <el-form class="login-container" label-position="left"
+             label-width="0px">
+      <h3 class="login_title">系统登录</h3>
+      <el-form-item>
+        <el-input type="text" v-model="loginForm.username"
+                  auto-complete="off" placeholder="账号"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-input type="password" v-model="loginForm.password"
+                  auto-complete="off" placeholder="密码"></el-input>
+      </el-form-item>
+      <el-form-item style="width: 100%">
+        <el-button type="primary" style="width: 100%;background: #505458;border: none" v-on:click="login">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </body>
 </template>
 
 <script>
@@ -22,8 +24,8 @@
     data () {
       return {
         loginForm: {
-          username: '',
-          password: ''
+          username: 'admin',
+          password: '123'
         },
         responseResult: []
       }
@@ -63,6 +65,17 @@
     margin: 0px auto 40px auto;
     text-align: center;
     color: #505458;
+  }
+  #poster {
+    background:url("../assets/eva.jpg") no-repeat;
+    background-position: center;
+    height: 100%;
+    width: 100%;
+    background-size: cover;
+    position: fixed;
+  }
+  body{
+    margin: 0px;
   }
 
 </style>
