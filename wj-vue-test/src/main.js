87057fe382be751c431931d0bdc24b38442feca0
jiaYF <jiajiajiajia10003@163.com>
2019-11-25 20:40:50 +0800

    前后端分离项目：前端工程：完整引入ElementUI。 安装命令：npm i element-ui -S

--- wj-vue-test/src/main.js
+++ wj-vue-test/src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 
 // 设置反向代理，前端请求默认发送到 http://localhost:8443/api
 var axios = require('axios')
@@ -11,6 +13,9 @@ axios.defaults.baseURL = 'http://localhost:8443/api'
 Vue.prototype.$axios = axios
 
 Vue.config.productionTip = false
+
+// 引入ElementUI
+Vue.use(ElementUI)
 
 /* eslint-disable no-new */
 new Vue({
