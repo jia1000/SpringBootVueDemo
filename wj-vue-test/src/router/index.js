f98a2bef56daf0d766f25d5080d70c59c5dd9ab9
Jia\Haha <jiajiajiajia10003@163.com>
2019-11-26 00:24:47 +0800

    前后端分离项目：前端工程：路由的配置。 并没有把首页的访问路径设置为 /home/index，仍然可以通过 /index 访问首页，这样配置其实是感受不到 /home 这个路径的存在的。之后再添加新的页面，可以直接在 children 中增添对应的内容。

--- wj-vue-test/src/router/index.js
+++ wj-vue-test/src/router/index.js
@@ -4,22 +4,34 @@ import Router from 'vue-router'
 // 导入刚才编写的组件
 import AppIndex from '@/components/home/AppIndex'
 import Login from '@/components/Login'
-
+import Home from '@/components/Home'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     // 下面都是固定的写法
+    {
+      path: '/home',
+      name: 'Home',
+      component: Home,
+      redirect: '/index',
+      children: [
+        {
+          path: '/index',
+          name: 'AppIndex',
+          component: AppIndex,
+          mata: {
+            requireAuth: true
+          }
+        }
+      ]
+    },
     {
       path: '/login',
       name: 'Login',
       component: Login
-    },
-    {
-      path: '/index',
-      name: 'AppIndex',
-      component: AppIndex
     }
   ]
 })
