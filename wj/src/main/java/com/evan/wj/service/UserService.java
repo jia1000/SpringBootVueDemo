861e4f7bbe4ec39487e0da75e655d74ca81e1c31
jiaYF <jiajiajiajia10003@163.com>
2019-11-25 20:17:04 +0800

    前后端分离项目：后端工程 ：增加UserService类，它是DAO的二次封装。

---
+++ wj/src/main/java/com/evan/wj/service/UserService.java
@@ -0,0 +1,35 @@
+package com.evan.wj.service;
+
+//实际上是对 UserDAO 进行了二次封装，
+// 一般来讲，我们在 DAO 中只定义基础的增删改查操作，
+// 而具体的操作，需要由 Service 来完成。
+// 当然，由于我们做的操作原本就比较简单，
+// 所以这里看起来只是简单地重命名了一下，
+// 比如把 “通过用户名及密码查询并获得对象” 这种方法命名为 get
+
+import com.evan.wj.dao.UserDAO;
+import com.evan.wj.pojo.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+@Service
+public class UserService {
+    @Autowired
+    UserDAO userDAO;
+
+    public boolean isExist(String username) {
+        User user = getByName(username);
+        return null!=user;
+    }
+
+    public User getByName(String username) {
+        return userDAO.findByUsername(username);
+    }
+
+    public User get(String username, String password){
+        return userDAO.getByUsernameAndPassword(username, password);
+    }
+
+    public void add(User user) {
+        userDAO.save(user);
+    }
+}
