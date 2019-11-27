2296a70cb940917556e19377acf4b2291b334457
jiaYF <jiajiajiajia10003@163.com>
2019-11-27 14:35:10 +0800

    前后端分离项目：后端工程：在LibraryController控制器类中，每个方法前面，都加上注解@CrossOrigin。完成跨域问题。

--- wj/src/main/java/com/evan/wj/controller/LibraryController.java
+++ wj/src/main/java/com/evan/wj/controller/LibraryController.java
@@ -13,23 +13,26 @@ public class LibraryController {
     @Autowired
     BookService bookService;
 
+    @CrossOrigin
     @GetMapping("/api/books")
     public List<Book> list() throws Exception {
         return bookService.list();
     }
 
+    @CrossOrigin
     @PostMapping("/api/books")
     public Book addOrUpdate(@RequestBody Book book) throws Exception {
         bookService.addOrUpdate(book);
         return book;
     }
 
+    @CrossOrigin
     @PostMapping("/api/delete")
     public void delete(@RequestBody Book book) throws Exception {
         bookService.deleteById(book.getId());
     }
 
-
+    @CrossOrigin
     @GetMapping("/api/categories/{cid}/books")
     public List<Book> listByCategory(@PathVariable("cid") int cid) throws Exception {
         if (0 != cid) {
